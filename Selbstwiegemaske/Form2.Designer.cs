06aba366a51287c05017b7e5ff2cd65cd669f19d
dagmar <dagmar.metzger@reko-software.de>
2020-10-29 14:58:52 +0100

    d

---
+++ Selbstwiegemaske/Form2.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Selbstwiegemaske
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthEdit1 = new DevExpress.XtraScheduler.UI.MonthEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.monthEdit1.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // monthEdit1
+            // 
+            this.monthEdit1.Location = new System.Drawing.Point(521, 113);
+            this.monthEdit1.Name = "monthEdit1";
+            this.monthEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.monthEdit1.Size = new System.Drawing.Size(100, 20);
+            this.monthEdit1.TabIndex = 0;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.monthEdit1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.monthEdit1.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraScheduler.UI.MonthEdit monthEdit1;
+    }
+}
